061e431398b2c0071ce29f64351e9823a2c54ec9
ShayalGeorgeK <shayalgeorge@gmail.com>
2026-05-08 13:20:05 +0530

    8th commit

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
         </ul>
 
         <div className='flex items-center gap-6'>
-            <img onClick={()=>setShowSearch(true)} src={assets. search_icon} className='w-5 cursor-pointer' alt="" />
+            <img onClick={()=>setShowSearch(true)} src={assets. search_icon} className='w-5 cursor-pointer' alt="" /> 
 
             <div className='group relative'>
                 
